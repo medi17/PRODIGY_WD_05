43f057274a4b83e0704bda7530f5391287aec0cb
Medan <medanabebe17@gmail.com>
2025-03-06 23:09:14 +0300

    seconnd API call function

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-import {ChangeEvent, JSX, useState } from 'react'
+import {ChangeEvent, JSX, useEffect, useState } from 'react'
 import {optionType} from "./types/types"
 
 const App = (): JSX.Element => {
 
      const [input, setInput] = useState<string>('');
-     const[options, setOptions] = useState<[]>([])
+     const [options, setOptions] = useState<[]>([]);
+     const [city, setCity] = useState<optionType | null>(null);
+
 
      const getOptions = (value: string) => {
-          
+
           const apiKey = import.meta.env.VITE_MY_API_KEY
+
           fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${apiKey}`)
                .then((res) => res.json())
                .then((data) => setOptions(data))
      }
+
      const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
           const value = e.target.value
           setInput(value)
@@ -23,6 +27,21 @@ const App = (): JSX.Element => {
           
      }
 
+     const chooseOption = (option: optionType) => {
+          setCity(option)
+
+          fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${option.lat}&lon=${option.lon}&units=metric&appid=${import.meta.env.VITE_MY_API_KEY}`)
+               .then((res) => res.json())
+               .then((data) => console.log({data}))
+     }
+
+     useEffect (() => {
+          if (city) {
+               setInput(city.name)
+               setOptions([])
+          }
+     }, [city])
+
      return (
           <div className="bg-[#0B0B29] text-white h-screen flex justify-center items-center">
                <div className="bg-[#21354D] text-center rounded-lg min-w-[350px] p-8 mx-10 md:max-w-[520px]">
@@ -32,7 +51,7 @@ const App = (): JSX.Element => {
                     </p>
                     <div className="relative mx-4 mt-8 rounded-lg flex justify-center items-center gap-2">
                          <input className="p-3 border-none bg-[#c6c6d9] w-full h-full rounded-lg text-[#0B0B29] focus:outline-none"
-                              type="text"
+                              type="text" 
                               placeholder='Enter a city'
                               value={input}
                               onChange={handleInputChange}
@@ -43,7 +62,9 @@ const App = (): JSX.Element => {
                          <ul className='absolute top-[40px] bg-[#c6c6d9] mr-50 ml-0 rounded-b-md md:mr-75'>
                               {options.map((option: optionType, index:number) => (
                                    <li className='text-black' key={option.name + '-' + index}>
-                                        <button className='text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-1 cursor-pointer'>
+                                        <button className='text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-1 cursor-pointer'
+                                                  onClick={() => chooseOption(option)}
+                                        >
                                              {option.name}
                                         </button>
                                    </li>
