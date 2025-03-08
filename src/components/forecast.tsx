744d9837ac0cba9e71a243c7a12d337c58a34983
Medan <medanabebe17@gmail.com>
2025-03-08 21:59:58 +0300

    modified forecast function

--- src/components/forecast.tsx
+++ src/components/forecast.tsx
@@ -1,5 +1,7 @@
 import {JSX} from 'react'
 import { forecastType } from "../types/types"
+import { getSunTime, getHumidity, getPop, getVisibility, getWindDirection } from "../helpers/helper"
+
 import Sunriseicon from "../assets/sunrise-svgrepo-com.svg"
 import Sunseticon from "../assets/sunset-svgrepo-com.svg"
 import Feelslike from "../assets/health-medical-medicine-termometer-svgrepo-com.svg"
@@ -17,13 +19,13 @@ const forecast = ({ data }: Props): JSX.Element => {
      const today = data.list[0]
      const Degree = ({temp}: {temp: number}) => (
           <span>
-               { temp}<sup>o</sup>
+               {temp}<sup>o</sup>
           </span>
      )
           
 
      return (
-          <div className="bg-[#21354D] text-center rounded-lg min-w-[350px] h-[90%] p-8 md:max-w-[520px] ">
+          <div className="bg-[#21354D] text-center rounded-lg w-[320px] h-[90%] p-8 md:w-[520px] ">
                <h1 className='text-2xl text-[#888290] font-bold'>{data.name} <span className='font-light text-[#9b9bb5]'>{data.country}</span></h1>
                <h2 className='text-4xl text-[#888290] font-extrabold'><Degree temp = {Math.round(today.main.temp)}/></h2>
                <h3 className='text-[14px]'>{today.weather[0].main} ({today.weather[0].description})</h3>
@@ -55,14 +57,14 @@ const forecast = ({ data }: Props): JSX.Element => {
                </section>
                
 
-               <div className="grid grid-cols-2 gap-4">
+               <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
                     <div className='bg-[#0b1b2e] p-4 rounded-lg'>
                          <span className='text-xs flex justify-center mb-3'><img className='sun w-10 h-10' src={Sunriseicon} alt={today.weather[0].description} /></span>
-                         <h4 className=''>{ new Date (data.sunrise * 1000).getHours()}</h4>
+                         <h4 className=''>{ getSunTime(data.sunrise)}</h4>
                     </div>
                     <div className='bg-[#0b1b2e] p-4 rounded-lg'>
                          <span className='text-xs flex justify-center mb-3'><img className='sun w-10 h-10' src={Sunseticon} alt={today.weather[0].description} /></span>
-                         <h4>{new Date (data.sunset * 1000).getHours()}</h4>
+                         <h4>{getSunTime(data.sunset)}</h4>
                     </div>
 
                     <div className='bg-[#0b1b2e] p-4 rounded-lg text-left'>
@@ -70,48 +72,48 @@ const forecast = ({ data }: Props): JSX.Element => {
                               <span className=''><img className='sun w-6' src={Wind} alt="wind" /></span>
                               <h5 className='text-[#888290] font-bold mt-1 text-xl ml-2'>Wind</h5>
                          </div>
-                         <h2 className = 'my-3 text-sm'>5 km/h</h2>
-                         <p>SW, gusts 10.1 km/h</p>
+                         <h2 className = 'my-3 text-sm'>{Math.round(today.wind.speed)} Km/h</h2>
+                         <p>{getWindDirection(today.wind.deg)}, gusts {(today.wind.gust).toFixed(1)} km/h</p>
                     </div>
                     <div className='bg-[#0b1b2e] p-4 rounded-lg text-left'>
                          <div className='flex items-center'>
                               <span className=''><img className='sun w-6' src={Feelslike} alt="termometer" /></span>
                               <h5 className='text-[#888290] font-bold mt-1 text-xl ml-2'>Feels Like</h5>
                          </div>
-                         <h2 className = 'my-3 text-sm'>5 km/h</h2>
-                         <p>SW, gusts 10.1 km/h</p>
+                         <h2 className = 'my-3 text-sm'><Degree temp = {Math.round(today.main.feels_like)} /></h2>
+                         <p>Feels { Math.round(today.main.feels_like) < Math.round(today.main.temp) ? "colder" : "warmer"}</p>
                     </div>
                     <div className='bg-[#0b1b2e] p-4 rounded-lg text-left'>
                          <div className='flex items-center'>
                               <span className=''><img className='sun w-6' src={Humidity} alt="humidity" /></span>
                               <h5 className='text-[#888290] font-bold mt-1 text-xl ml-2'>Humidity</h5>
                          </div>
-                         <h2 className = 'my-3 text-sm'>5 km/h</h2>
-                         <p>SW, gusts 10.1 km/h</p>
+                         <h2 className = 'my-3 text-sm'>{today.main.humidity} %</h2>
+                         <p>{getHumidity(today.main.humidity)}</p>
                     </div>
                     <div className='bg-[#0b1b2e] p-4 rounded-lg text-left'>
                          <div className='flex items-center'>
                               <span className=''><img className='sun w-6' src={Precipitation} alt="ice-picture" /></span>
                               <h5 className='text-[#888290] font-bold mt-1 text-xl ml-2'>Precipitation</h5>
                          </div>
-                         <h2 className = 'my-3 text-sm'>5 km/h</h2>
-                         <p>SW, gusts 10.1 km/h</p>                         
+                         <h2 className = 'my-3 text-sm'>{Math.round(today.pop *  1000)} %</h2>
+                         <p>{getPop(today.pop)}, clouds at {today.clouds.all} %</p>                         
                     </div>
                     <div className='bg-[#0b1b2e] p-4 rounded-lg text-left'>
                          <div className='flex items-center'>
                               <span className=''><img className='sun w-6' src={Pressure} alt="pessure-scale" /></span>
                               <h5 className='text-[#888290] font-bold mt-1 text-xl ml-2'>Pressure</h5>
                          </div>
-                         <h2 className = 'my-3 text-sm'>5 km/h</h2>
-                         <p>SW, gusts 10.1 km/h</p>                         
+                         <h2 className = 'my-3 text-sm'>{today.main.pressure} hPa</h2>
+                         <p>{Math.round(today.main.pressure) < 1023 ? "Lower" : "Higher"} than standard</p>                         
                     </div>
                     <div className='bg-[#0b1b2e] p-4 rounded-lg text-left'>
                          <div className='flex items-center'>
                               <span className=''><img className='sun w-6' src={Visibility} alt="eye" /></span>
                               <h5 className='text-[#888290] font-bold mt-1 text-xl ml-2'>Visibility</h5>
                          </div>
-                         <h2 className = 'my-3 text-sm'>5 km/h</h2>
-                         <p>SW, gusts 10.1 km/h</p>                         
+                         <h2 className = 'my-3 text-sm'>{(today.visibility / 1000).toFixed()} km</h2>
+                         <p>{getVisibility(today.visibility)}</p>                         
                     </div>
                </div>
 
--- src/components/search.tsx
+++ src/components/search.tsx
@@ -11,28 +11,36 @@ type Props = {
 
 const Search = ({input, handleInputChange, onSubmit, options, chooseOption}: Props):JSX.Element => {
 
-     return (
-          <div className="relative mx-4 mt-8 rounded-lg flex justify-center items-center gap-2">
-               <input className="p-3 border-none bg-[#c6c6d9] w-full h-full rounded-lg text-[#0B0B29] focus:outline-none"
-                    type="text" 
-                    placeholder='Enter a city'
-                    value={input}
-                    onChange={handleInputChange}
-               />
+     
 
-               <button className="py-2 px-4 bg-[#0B0B29] mr-2 rounded-lg cursor-pointer hover:bg-[#21354D] hover:border hover:border-[#c6c6d9]"
-                    onClick={onSubmit}
-               >search</button>
-               <ul className='absolute top-[40px] bg-[#c6c6d9] mr-50 ml-0 rounded-b-md md:mr-75'>
-                    {options.map((option: optionType, index:number) => (
-                         <li className='text-black' key={option.name + '-' + index}>
-                              <button className='text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-1 cursor-pointer'
-                                        onClick= {() => chooseOption(option)} >
-                                   {option.name}
-                              </button>
-                         </li>
-                    ))}
-               </ul>
+     return (
+          <div className="bg-[#21354D] text-center rounded-lg min-w-[350px] p-8 mx-10 md:max-w-[520px]">
+               <h1 className="text-3xl font-medium my-6  text-[#888290]">Weather <span className=" text-[#9b9bb5] font-black ">Forecast</span></h1>
+               <p className="text-sm font-extralight">Enter below a place you want to know the
+                    weather of and select an option from the dropdown
+               </p>
+               <div className="relative mx-4 mt-8 rounded-lg flex justify-center items-center gap-2">
+                    <input className="p-3 border-none bg-[#c6c6d9] w-full h-full rounded-lg text-[#0B0B29] focus:outline-none"
+                         type="text" 
+                         placeholder='Enter a city'
+                         value={input}
+                         onChange={handleInputChange}
+                    />
+
+                    <button className="py-2 px-4 bg-[#0B0B29] mr-2 rounded-lg cursor-pointer hover:bg-[#21354D] hover:border hover:border-[#c6c6d9]"
+                         onClick={onSubmit}
+                    >search</button>
+                    <ul className='absolute top-[40px] bg-[#c6c6d9] mr-50 ml-0 rounded-b-md md:mr-75'>
+                         {options.map((option: optionType, index:number) => (
+                              <li className='text-black' key={option.name + '-' + index}>
+                                   <button className='text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-1 cursor-pointer'
+                                             onClick= {() => chooseOption(option)} >
+                                        {option.name}
+                                   </button>
+                              </li>
+                         ))}
+                    </ul>
+               </div>
           </div>
 
 
